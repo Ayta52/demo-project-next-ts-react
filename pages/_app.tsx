9a04f5ca1ef880a47734f28ff92001e18fcf85e7
Andrey Sokolov <ayta.sokolov@yandex.ru>
2023-07-13 15:43:32 +0300

    overwriting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import Head from 'next/head'
-import type { AppProps } from 'next/app'
-import '@/styles/globals.css'
+import React from 'react';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <React.Fragment>
+    <>
       <Head>
         <title>MyTop - лучший топ</title>
-        <link rel='icon' href='/favicon.ico' />
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href='https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap'
-          rel='stylesheet'
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap"
         />
       </Head>
       <Component {...pageProps} />
-    </React.Fragment>
-  )
+    </>
+  );
 }
